ce4576f3693063666ea881c5d5f1005e6d192cbc
shrilak412 <72849354+shrilak412@users.noreply.github.com>
2021-08-26 18:44:58 +0530

    Add files via upload

---
+++ precache-manifest.30b43236adc86a8d76c92fc4043810bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a121cc25b58451defa095d08c4a66e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c25edd2ac515ab80108d",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "78778ebbb5a3a8337fd7",
+    "url": "/static/css/main.d8f8b1af.chunk.css"
+  },
+  {
+    "revision": "c25edd2ac515ab80108d",
+    "url": "/static/js/2.3f4757d2.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.3f4757d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78778ebbb5a3a8337fd7",
+    "url": "/static/js/main.4ebc8227.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
